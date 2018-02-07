99d0245596a91c267130f2ec2100b15693c02468
Katie Maxwell <katie@codeinstitute.net>
2018-02-07 16:48:53 +0000

    questions appearing with answer entry on selecting 1 from menu

--- quiz.py
+++ quiz.py
@@ -5,15 +5,51 @@ def show_menu():
     
     option = input("Enter your choice: ")
     return int(option)
+
+def ask_questions():
+    print("You want to ask questions")
+    with open("questions.txt") as f:
+        lines = f.read().split("\n")
+        
+    numbered_lines = list(enumerate(lines))
     
+    questions = []
+    answers = []
+    
+    for n,t in numbered_lines:
+        if n%2 == 0 :
+            questions.append(t)
+        else:
+            answers.append(t)
+        
+    q_and_a = list(zip(questions, answers))
+    
+    for q,a in q_and_a:
+        guess = input(q)
+        if guess == a:
+            print("Correct!")
+        else:
+            print("WRONG!")
+    
+def add_question():
+    print("You want to add a question")
+    question = input("Enter a question: ")
+    answer = input("Enter the answer: ")
+    
+    with open("questions.txt", "a") as f:
+        f.write(question + "\n")
+        f.write(answer + "\n")
+
+    
+
 def game_loop():
     while True:
         option = show_menu()
         
         if option == 1:
-            print("You want to ask the questions")
+            ask_questions()
         if option == 2:
-            print("You want to add a question")
+            add_question()
         if option == 3:
             break
         
@@ -21,7 +57,3 @@ game_loop()
 
 
 
-# with open("questions.txt") as f:
-#     lines = f.read()
-
-# print(lines)
